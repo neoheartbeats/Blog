055746aeac9b301edaff22325b71c9d3bb9a17e6
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-10-30 15:22:02 +0800

    Updated files

--- source/conf.py
+++ source/conf.py
@@ -1,25 +1,53 @@
+import alabaster
+
 ### Project properties
 project = 'Scheilya ME'
 copyright = '2022, Ilya.w'
 author = 'Ilya.w'
 
 ### General configurations
-extensions = []
+extensions = ['alabaster']
 templates_path = ['_templates']
 exclude_patterns = []
 
 ### `HTML' output configurations
-## `CSS' related settings 
+## `CSS' related settings
 html_theme = 'alabaster'
+html_theme_path = [alabaster.get_path()]
 html_static_path = ['_static']
+html_theme_options = {
+    'description': "SCHEilya.ME",
+    'logo': 'logo.png',
+    'fixed_sidebar': 'true',
+    'show_related': 'true',
+    'show_powered_by': 'false',
+    'touch_icon': 'logo.png',
+    'base_bg': "#000000",
+    'base_text': "#dddddd",
+    'hr_border': "#dddddd",
+    'body_bg': "#000000",
+    'body_text': "#dddddd",
+    'shadow' : "#000000",
+    'link': "#6ae4b9",
+    'link_hover': "#f78fe7",
+    'sidebar_header': "#f78fe7",
+    'sidebar_text': "#dddddd",
+    'sidebar_link': "#6ae4b9",
+    'code_bg': "#000000",
+    'code_text': "#00c06f",
+    'code_hover': "#323232",
+    'code_highlight': "#00c06f",
+    'code_font_family': "Fira Code",
+    'font_family': "Fira Sans",
+    'head_font_family': "Fira Sans Bold",
+    'caption_font_family': "Fira Sans"
+}
 
 ## Site identity settings
-# Icons
-html_logo = './docs/img/logo.png'
 html_favicon = None
 
 # Text
-html_title = 'ILYA ME'
+html_title = 'SCHEilya.ME'
 
 ## Source file names
 root_doc = 'index'
@@ -28,5 +56,5 @@ source_suffix = '.rst'
 ## Ignored source files
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-## Syntax highlighting style
-pygments_style = 'sphinx'
+def setup(app):
+    app.add_css_file('styles.css')
